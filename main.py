dce1becf1bca2057c009e98e17925fa8e390d894
mcnabbian <ianwmcnabb@gmail.com>
2020-03-12 10:50:34 -0700

    added receiver and sender prompts. Sends plain text email.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import smtplib, ssl
 import getpass
 
-port = 465 # For gmail SSL
-password = getpass.getpass("Type your password and hit enter: ") # so the pass doesn't show up in my code
+sender = input("Type the gmail address you'd like to send from: ")
+password = getpass.getpass("Type your password and hit enter: ")  # so pass doesn't show up in code
+receiver = input("Type the gmail address you'd like to receive the email: ")
 
+message = """\
+Subject: TEST SUBJECT
+
+This is an automated message!"""
+
+port = 465 # For gmail SSL
 context = ssl.create_default_context() # something that helps establish ssl connection
 
 with smtplib.SMTP_SSL('smtp.gmail.com', port=port, context=context) as server:
-    server.login('mcnabbiantesting@gmail.com', password)
-    # TODO: send email here
+    try:
+        server.login(sender, password)
+        server.sendmail(sender, receiver, message)
+        print("Successfully sent email!")
+    except smtplib.SMTPAuthenticationError as e:
+        print("Invalid login. Username or Password incorrect.")
