28216e41ea0be0cc55de06270c60da658967af96
mcnabbian <ianwmcnabb@gmail.com>
2020-03-19 10:50:28 -0700

    adjusted frame and centered questions. Separated code out into new create_questions method.

--- gui.py
+++ gui.py
@@ -16,6 +16,7 @@ def save_entry():
     with open('journal_entries.txt', 'a+') as f:
         # .get the input in text widget at the first line, '0th' character, then read until the end
         f.write("\n" + get_date_time() + "\n" + text_box.get('1.0', 'end-1c'))
+    # TODO: fix method to save all entries
 
 
 def popup():
@@ -25,47 +26,50 @@ def popup():
         root.destroy()
 
 
+def create_questions():
+    with open('questions.txt', 'r') as f:
+        for line in f:
+            # initialize widgets
+            question = Label(frame, text=line[:-1])  # cut last char from string because it's a '/n'
+            text_box = Text(frame, height=6, width=35, borderwidth=5,
+                            relief="groove", font=("Times", 14), wrap=WORD)
+
+            # add widgets to frame
+            question.pack(side="top")
+            text_box.pack(side="top")
+
 # def submit():
 #     """Command for the submit button. Ends the script and saves entry to database."""
 #     # TODO: Add save entry to database functionality.
 #
 
 # Create tk object
-root = Tk()
 
-canvas = Canvas(root)  # TODO: What is a canvas and why do I add a frame to it?
+
+# initialize tkinter
+root = Tk()
+root.geometry('510x500')
+
+# initialize canvas widget within root
+canvas = Canvas(root)
 scroll_y = Scrollbar(root, orient='vertical', command=canvas.yview)
-frame = Frame(root)
+# initialize frame within
+frame = Frame(root, bd='25', padx='75')
 
 # Create desired widgets
-with open('questions.txt', 'r') as f:
-    for line in f:
-
-        # initialize widgets
-        question = Label(frame, text=line[:-1])  # cut last char from string because it's a '/n'
-        text_box = Text(frame, height=6, width=35, borderwidth=5,
-                        relief="groove", font=("Times", 14), wrap=WORD)
-
-        # add widgets to frame
-        question.pack(side="top")
-        text_box.pack(side="top")
+create_questions()
 button = Button(frame, text="Submit", padx=35, command=popup)
-button.pack(side="bottom")
 
 # create new window, put frame in canvas
 canvas.create_window(0, 0, anchor='nw', window=frame)
-canvas.update_idletasks()  # TODO: not really sure what this does
+canvas.update_idletasks()  # not sure what this really does but not having it breaks the program
 canvas.configure(scrollregion=canvas.bbox('all'),
-                 yscrollcommand=scroll_y.set)  # TODO: scroll region currently only on the scroll bar, idk why
+                 yscrollcommand=scroll_y.set)
 
+# pack widgets
 canvas.pack(fill='both', expand=True, side='left')
 scroll_y.pack(fill='y', side='right')
-# Add widgets to window
-# question.pack(side="top")
-# text_box.pack(side="top")
-# scroll.pack(side=RIGHT, fill=Y)
-# button = Button(frame, text="Submit", padx=35, command=popup)
-# button.pack(side="bottom")
+button.pack(side="bottom")
 
 # Loop script to constantly update and keep running
 root.mainloop()
